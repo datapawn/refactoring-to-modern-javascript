b1b8a52f14fe1af16600b195d67341302c18d9a9
Raju Gandhi <raju.gandhi@gmail.com>
2020-02-24 19:06:18 -0500

    finish default-params exercises

--- src/020-default-params/default-and-rest-params.js
+++ src/020-default-params/default-and-rest-params.js
@@ -1,42 +1,29 @@
-
-function rectangleArea(x, y) {
-  if (!x) x = 10;
-  if (!y) y = x;
-
+/* eslint-disable no-restricted-syntax, prefer-arrow-callback */
+function rectangleArea(x = 10, y = x) {
   return x * y;
 }
 
-function validTriangle(a, b) {
-  if (!a) a = 10;
-  if (!b) b = a;
-  var c = +Math.hypot(a, b).toFixed(2);
+function validTriangle(a = 10, b = a) {
+  const c = +Math.hypot(a, b).toFixed(2);
 
   return [a, b, c];
 }
 
-function appendToArray(value, arr) {
-  if (!arr) arr = [];
+function appendToArray(value, arr = []) {
   arr.push(value);
   return arr;
 }
 
-function add(/** args */) {
-  var result = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    result += arguments[i];
-  }
-  return result;
+function add(...nums) {
+  return nums.reduce(function sum(acc, n) {
+    return acc + n;
+  }, 0);
 }
 
-function multiplyWithFactor(factor /** , nums */) {
-  if (!factor) factor = 1;
-  // TODO: Find a better way to do this. Here is a helpful link
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-  var nums = Array.prototype.slice.call(arguments);
-  var result = factor;
-  // TODO: Why do we start at 1 here?
-  for (var i = 1; i < nums.length; i++) {
-    result *= nums[i];
+function multiplyWithFactor(factor = 1, ...args) {
+  let result = factor;
+  for (const n of args) {
+    result *= n;
   }
   return result;
 }
